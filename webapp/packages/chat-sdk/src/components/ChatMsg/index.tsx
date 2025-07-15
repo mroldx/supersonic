34fca7e2546a37983c39f98bfc13c01c708dc40b
WDEP <128273529+WDEP-good@users.noreply.github.com>
2025-07-15 10:17:22 +0800

    (fix)(webapp) 修改页面不显示推荐下钻维度问题 (#2332)
    (cherry picked from commit 62b9db679123edc20f85ec5e2f6d6ddf100dd960)

--- webapp/packages/chat-sdk/src/components/ChatMsg/index.tsx
+++ webapp/packages/chat-sdk/src/components/ChatMsg/index.tsx
@@ -351,7 +351,7 @@ const ChatMsg: React.FC<Props> = ({
     entityName !== undefined;
 
   const existDrillDownDimension =
-    queryMode.includes('METRIC') &&
+  (queryMode.includes('METRIC') || queryMode === 'LLM_S2SQL')&&
     getMsgContentType() !== MsgContentTypeEnum.TEXT &&
     !isEntityMode;
 
